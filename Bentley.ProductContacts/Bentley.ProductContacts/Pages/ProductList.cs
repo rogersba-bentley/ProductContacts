48ddd97fbef00dfeab71bc026d5b5ba17435bca6
Ben.Rogers <ben.rogers@bentley.com>
2016-08-15 07:47:32 -0400

    Add search bar to product list

--- Bentley.ProductContacts/Bentley.ProductContacts/Pages/ProductList.cs
+++ Bentley.ProductContacts/Bentley.ProductContacts/Pages/ProductList.cs
@@ -12,28 +12,48 @@ namespace Bentley.ProductContacts
 
         private ProductContactDatabase m_database;
         private ListView m_productList;
+        private SearchBar m_search;
 
         public ProductListPage (ProductContactDatabase database)
             {
             m_database = database;
             Title = "Random Thoughts";
             var products = m_database.GetProductLines ();
-
             m_productList = new ListView ();
+
+            m_search = new SearchBar () { Placeholder = "Search for Product" };
+            m_search.TextChanged += (sender, e) => FilterProducts (m_search.Text);
+            m_search.SearchButtonPressed += (sender, e) => {
+                FilterProducts (m_search.Text);
+            };
+
             m_productList.ItemsSource = products;
             m_productList.ItemTemplate = new DataTemplate (typeof (TextCell));
             m_productList.ItemTemplate.SetBinding (TextCell.TextProperty, "Name");
-            m_productList.ItemTemplate.SetBinding (TextCell.DetailProperty, "Name");
+            //m_productList.ItemTemplate.SetBinding (TextCell.DetailProperty, "Name");
 
-            //var toolbarItem = new ToolbarItem
-            //    {
-            //    Text = "Add",
-             //   Command = new Command (() => Navigation.PushAsync (new ThoughtEntryPage (this, database)))
-            //    };
+            var stack = new StackLayout ()
+                {
+                Children =
+                    {
+                    m_search,
+                    m_productList
+                    }
+                };
 
-//            ToolbarItems.Add (toolbarItem);
+            Content = stack;
+            }
 
-            Content = m_productList;
+        private void FilterProducts (string search)
+            {
+            m_productList.BeginRefresh ();
+
+            if ( string.IsNullOrWhiteSpace (search) )
+                m_productList.ItemsSource = m_database.GetProductLines ();
+            else
+                m_productList.ItemsSource = m_database.SearchProductLines (search);
+ 
+            m_productList.EndRefresh ();
             }
 
         public void Refresh ()
